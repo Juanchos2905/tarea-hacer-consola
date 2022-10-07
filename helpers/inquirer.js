d62fbfa28f113d735a7cda7babdf0caf8c4a3701
Panditax0902 <juanchos2905@gmail.com>
2022-10-06 20:49:31 -0500

    segunda entrega

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,42 +1,5 @@
 const inquirer = require('inquirer')
 const colors = require('colors')
-const preguntas = [
-  {
-    type: 'list',
-    name: 'opcion',
-    message: '¿Qué desea hacer?',
-    choices: [
-      {
-        value: 1,
-        name: `${'1.'.red} Crear una tarea`,
-      },
-      {
-        value: 2,
-        name: `${'2.'.red} Listar tareas`,
-      },
-      {
-        value: 3,
-        name: `${'3.'.red} Listar tareas completadas`,
-      },
-      {
-        value: 4,
-        name: `${'4.'.red} Listar tareas pendientes`,
-      },
-      {
-        value: 5,
-        name: `${'5.'.red} Completar tarea(s)`,
-      },
-      {
-        value: 6,
-        name: `${'6.'.red} Borrar tarea`,
-      },
-      {
-        value: 7,
-        name: `${'0.'.red} Salir`,
-      },
-    ],
-  },
-]
 
 const inquirerMenu = async () => {
   console.clear()
@@ -45,6 +8,43 @@ const inquirerMenu = async () => {
     colors.magenta('||'.yellow + '  Seleccione una opción   ' + '||'.yellow)
   )
   console.log(colors.yellow('=============================='))
+  const preguntas = [
+    {
+      type: 'list',
+      name: 'opcion',
+      message: '¿Qué desea hacer?',
+      choices: [
+        {
+          value: 1,
+          name: `${'1.'.red} Crear una tarea`,
+        },
+        {
+          value: 2,
+          name: `${'2.'.red} Listar tareas`,
+        },
+        {
+          value: 3,
+          name: `${'3.'.red} Listar tareas completadas`,
+        },
+        {
+          value: 4,
+          name: `${'4.'.red} Listar tareas pendientes`,
+        },
+        {
+          value: 5,
+          name: `${'5.'.red} Completar tarea(s)`,
+        },
+        {
+          value: 6,
+          name: `${'6.'.red} Borrar tarea`,
+        },
+        {
+          value: 7,
+          name: `${'0.'.red} Salir`,
+        },
+      ],
+    },
+  ]
 
   const { opcion } = await inquirer.prompt(preguntas)
   return opcion
@@ -62,7 +62,26 @@ const pausa = async () => {
   await inquirer.prompt(question)
 }
 
+const leerInput = async (message) => {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message, // En realidad es message : message solo que el vs lo entiende
+      validate(value) {
+        if (value.length === 0) {
+          return 'Por favor ingrese un valor'
+        }
+        return true
+      },
+    },
+  ]
+  const { desc } = await inquirer.prompt(question)
+  return desc
+}
+
 module.exports = {
   inquirerMenu,
   pausa,
+  leerInput,
 }
